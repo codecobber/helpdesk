d944839eb1e155796cad03c08e9f10f29b901ea8
codecobber <codecobber@gmail.com>
2019-03-08 15:45:18 +0000

    Added check in edit for duplicates

--- template_create.php
+++ template_create.php
@@ -1,6 +1,6 @@
- <?php 
+ <?php
   $page = "create";
-  include('includes/header.inc.php'); 
+  include('includes/header.inc.php');
 ?>
 
         <hr>
@@ -11,15 +11,15 @@
                 <div class="large-6 columns">
                 <h2><?php get_page_title(); ?></h2>
                 <hr>
-                
-                
-    
+
+
+
                         <?php
                         // define variables and set to empty values
                         $titleErr = $datepickerErr = $timeErr = $impactErr = $emailErr = $ticketnoErr = $statusErr = $detailsErr = $ticktypeErr = "";
                         $ticketno = $time = $datepicker = $title = $email = $status =$impact = $details = $ticktype = "";
 
-                       
+
 
                         function test_input($data) {
                           $data = trim($data);
@@ -29,7 +29,7 @@
                         }
 
 
-                        //Testing inputs ------------------------ 
+                        //Testing inputs ------------------------
 
                         if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -41,7 +41,7 @@
                           } else {
                             $datepicker = test_input($_POST["datepicker"]);
                           }
-                          
+
                           if (empty($_POST["title"]) || ctype_space($_POST["title"])) {
                             $titleErr = "Title required";
                             $checkFlag =1;
@@ -49,10 +49,10 @@
                             $title = test_input($_POST["title"]);
                             // check if name only contains letters and whitespace
                             if (!preg_match("/[a-zA-Z0-9\s]+/",$title)) {
-                              $titleErr = "Only letters and white space allowed"; 
+                              $titleErr = "Only letters and white space allowed";
                             }
                           }
-                          
+
                           if (empty($_POST["ticketno"]) || ctype_space($_POST["ticketno"])) {
                             $ticketnoErr = "Ticket number required";
                             $checkFlag =1;
@@ -60,7 +60,7 @@
                             $ticketno = test_input($_POST["ticketno"]);
                             // check if name only contains letters and whitespace
                             if (!preg_match("/^[a-zA-Z0-9 ]*$/",$ticketno)) {
-                              $ticketnoErr = "Only letters, numbers and white space allowed"; 
+                              $ticketnoErr = "Only letters, numbers and white space allowed";
                             }
                           }
 
@@ -71,7 +71,7 @@
                             $time = test_input($_POST["time"]);
                             // check if name only contains letters and whitespace
                             if (!preg_match("/^[\d]{2}:[\d]{2}$/",$time)) {
-                              $timeErr = "Please enter in the format - dd:dd"; 
+                              $timeErr = "Please enter in the format - dd:dd";
                             }
                           }
 
@@ -82,7 +82,7 @@
                             $email = test_input($_POST["email"]);
                             // check if e-mail address is well-formed
                             if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                              $emailErr = "Invalid email format"; 
+                              $emailErr = "Invalid email format";
                             }
                           }
 
@@ -123,24 +123,24 @@
 
 
                           if($checkFlag == 0){
-                            
+
                             $existsFlag = 0;
 
-                            
+
                             //save file
                             $filesLoc = "theme/helpdesk/dataSearch/".$_SESSION['pStatus']."/data.json";
                             $dataFile = file_get_contents($filesLoc);
                             $decodeJson = json_decode($dataFile);
 
-                            
+
                             //Loop through array and check for duplicate id
                             foreach ($decodeJson as $skey => $svalue) {
                                if($decodeJson[$skey]->id == $ticketno){
                                 $existsFlag = 1;
                                }
-                             } 
+                             }
 
-                            
+
                              if($existsFlag == 0){
 
                               $mymessage = "<p id='success'>Ticket created</p>";
@@ -157,21 +157,21 @@
                               $newTicket->details =$details;
                               $newTicket->status = $status;
                               $newTicket->impact = $impact;
-               
+
                               //add object to array
                               array_push($decodeJson, $newTicket);
-                             
+
                               //decode and pretty print
                               $jDataStr = json_encode($decodeJson,JSON_PRETTY_PRINT);
-                              
+
                               //store the new data back to file
                               file_put_contents($filesLoc,$jDataStr);
                              }
                              else{
-                              $mymessage = "<p id='success' style='background-color:#b33434'>Ticket ID already exists</p>";
+                              $mymessage = "<p class='errorRed'>Ticket ID already exists</p>";
                              }
 
-                            
+
                           }
                       }
 
@@ -180,53 +180,53 @@
 
                 <!-- Form -------------------------- -->
 
-               
+
                 <p><span class="form_error">* Required fields</span></p>
-                <form id="ticketForm" method="post" action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']);?>">  
-                  
+                <form id="ticketForm" method="post" action="<?php echo htmlspecialchars($_SERVER['REQUEST_URI']);?>">
+
                   <label style='display: inline;'>Creation Date:</label>
                   <span class="form_error"> * <?php echo $datepickerErr;?></span>
                   <input id="datepicker" style='display: inline;' type="text" name="datepicker" value="<?php echo $datepicker;?>">
-                  
+
                   <br><br>
 
                   <label style='display: inline;'>Time: <small style='color:#cacaca'>eg: 13:10</small></label>
                   <span class="form_error"> * <?php echo $timeErr;?></span>
                   <input id="time" style='display: inline;' type="text" name="time" value="<?php echo $time;?>">
-                  
+
                   <br><br>
 
                   <label style='display: inline;'>E-mail: </label>
                   <span class="form_error"> * <?php echo $emailErr;?></span>
                   <input type="text" name="email" value="<?php echo $email;?>">
-    
+
                   <br><br>
 
-                  
+
 
                   <label style='display: inline;'>Title: </label>
                   <span class="form_error"> * <?php echo $titleErr;?></span>
                   <input type="text" name="title" value="<?php echo $title;?>" maxlength="120">
-                  
+
                   <br><br>
 
                   <label style='display: inline;'>Ticket No: </label>
                   <span class="form_error"> * <?php echo $ticketnoErr;?></span>
                   <input type="text" name="ticketno" value="<?php echo $ticketno;?>" maxlength="15">
-                  
+
                   <br><br>
 
                   <label style='display: inline;'>Ticket Type: </label>
-                  
+
                   <input type="radio" name="ticktype" <?php if (isset($ticktype) && $ticktype=="1") echo "checked";?> value="1">P1
                   <input type="radio" name="ticktype" <?php if (isset($ticktype) && $ticktype=="2") echo "checked";?> value="2">P2
                   <span class="form_error"> * <?php echo $ticktypeErr;?></span>
-                  <br><br> 
+                  <br><br>
 
                   <label style='display: inline;'>Ticket Status: </label>
-                  
+
                   <input type="radio" name="status" <?php if (isset($status) && $status=="1") echo "checked";?> value="1">Red
-                  <input type="radio" name="status" <?php if (isset($status) && $status=="2") echo "checked";?> value="3">Green  
+                  <input type="radio" name="status" <?php if (isset($status) && $status=="2") echo "checked";?> value="3">Green
                   <span class="form_error"> * <?php echo $statusErr;?></span>
                   <br><br>
 
@@ -235,18 +235,18 @@
                   <span class="form_error"> * <?php echo $detailsErr;?></span>
                   <br>
                   <textarea name="details" rows="5" cols="40"><?php echo $details;?></textarea>
-                  
-                  <br><br>
-                  
 
-                  <label style='display: inline;'>End user impact:</label> 
+                  <br><br>
+
+
+                  <label style='display: inline;'>End user impact:</label>
                   <span class="form_error"> * <?php echo $impactErr;?></span>
                   <br>
                   <textarea name="impact" rows="5" cols="40"><?php echo $impact;?></textarea>
-                  
-                  <br><br> 
 
-                  <input class="button" type="submit" name="submit" value="Submit">  
+                  <br><br>
+
+                  <input class="button" type="submit" name="submit" value="Submit">
                 </form>
 
                 </div>
@@ -254,17 +254,17 @@
                 <div class="large-6 columns">
                 <h2>Remember!</h2>
                 <hr>
-                <p>When inputting data, try to be as clear as possible. 
+                <p>When inputting data, try to be as clear as possible.
                 Add as much information as is required. Don&#39;t be vague.</p>
                 <p>The traffic lights notification is there to provide a visual indication of the progress of the ticket.</p>
                 <p>Initially, the ticket will be set to red as the issue is new and has not been dealt with. Later you can edit the status and change the colour accordingly. </p>
-                
+
                 <div><?php if(isset($mymessage)){echo $mymessage;} ?></div>
 
                 </div>
 
             </div>
 
-        </div>     
+        </div>
     </div>
    <?php include('includes/footer.inc.php'); ?>
